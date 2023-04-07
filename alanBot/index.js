8ff580defd518abeb189e5123cfca46d0b5417df
comfort 3 <comforta@usf.edu>
2023-04-07 16:44:00 -0400

    flesh-out-uldpform-workflow

--- alanBot/index.js
+++ alanBot/index.js
@@ -93,13 +93,35 @@ const date = current_date();
         // get student user from database
         const snapshot = await database.ref('/' + u_number).once('value');
         const student = snapshot.val();
-    
+
         // if the student has a valid (u-id, pin), populate form fields with their info
-        if (student && student.pin_number == pin) {
+        if (student && student.pin_number == pin && student.uldp == 'FALSE') {
             last_name_field.setText(student.last_name);
             first_name_field.setText(student.first_name);
             u_number_field.setText('U' + student.u_number);
             date_field.setText(date);
+
+            // check the student's major to check the correct box
+            switch (student.major) {
+                case 'cpe':
+                    const cpe_field = form.getCheckBox('Button7');
+                    cpe_field.check();
+                  break;
+                case 'cs':
+                    const cs_field = form.getCheckBox('Button8');
+                    cs_field.check();
+                  break;
+                case 'cys':
+                    const cys_field = form.getCheckBox('Button9');
+                    cys_field.check();
+                    break;
+                case 'it':
+                    const it_field = form.getCheckBox('Button10');
+                    it_field.check();
+                    break;
+                default:
+                    break;
+              }
     
             // save the modified PDF to a file
             const newPdfBytes = await pdfDoc.save();
@@ -109,12 +131,19 @@ const date = current_date();
                 "richContent":[
                     [
                         {
+                            "type": "description",
+                            "title": "Hi " + student.first_name + ",",
+                            "text": [
+                              "This link will take you to your auto-filled ULDP form. Please make sure to sign it and check it for any errors before submitting."
+                            ]
+                        },
+                        {
                             "type": "button",
                             "icon": {
                               "type": "download",
                               "color": "#006747"
                             },
-                            "text": "Auto-filled ULDP Form",
+                            "text": student.first_name + "'s ULDP Form",
                             "link": "https://san-francesco.github.io/alanBot/auto-fill-uldp.pdf",
                           }, 
                           {
@@ -132,9 +161,71 @@ const date = current_date();
                 ]
             }
             agent.add(new dfff.Payload(agent.UNSPECIFIED, payloadData, {sendAsMessage: true, rawPayload: true }))            
+        } else if (student && student.pin_number == pin && student.uldp == 'TRUE') { 
+            var payloadData = {
+                "richContent":[
+                    [
+                        {
+                            "type": "description",
+                            "title": "You're already considered an Upper Level student.",
+                            "text": [
+                              "According to our database, you're already an upper level student. You can double check this by navigating to DegreeWorks and verifying that you have the ULDP attribute. If you think this is an error, please contact your advisor.",
+                            ]
+                        },
+                          {
+                            "options": [
+                                {
+                                    "text": "Who is my advisor?"
+                                },
+                                {
+                                  "text": "Email my advisor"
+                                },
+                                {
+                                  "text": "I need something else"
+                                },
+                                {
+                                  "text": "Bye!"
+                                }
+                              ],
+                              "type": "chips"
+                          }
+                    ]
+                ]
+            }
+            agent.add(new dfff.Payload(agent.UNSPECIFIED, payloadData, {sendAsMessage: true, rawPayload: true }))            
         }
         else {
-            agent.add('Unfortunately, your UID and pin are not valid. Please try again or contact USF IT for help.');
+            var payloadData = {
+                "richContent":[
+                    [
+                        {
+                            "type": "description",
+                            "title": "Invalid PIN or U Number",
+                            "text": [
+                              "Unfortunately, your UID and pin are not valid. Please try again or contact USF IT for help.",
+                            ]
+                        },
+                          {
+                            "options": [
+                                {
+                                    "text": "Auto-fill my ULDP Form"
+                                },
+                                {
+                                    "text": "I don't know my PIN or UID"
+                                },
+                                {
+                                  "text": "I need something else"
+                                },
+                                {
+                                  "text": "Bye!"
+                                }
+                              ],
+                              "type": "chips"
+                          }
+                    ]
+                ]
+            }
+            agent.add(new dfff.Payload(agent.UNSPECIFIED, payloadData, {sendAsMessage: true, rawPayload: true }))            
         }
     
     }
