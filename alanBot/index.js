7ab2b3e5073e0d4083b9f4d0782ef2d1d1cae369
comfort 3 <comforta@usf.edu>
2023-04-09 21:48:44 -0400

    Form-Filling is Complete!

--- alanBot/index.js
+++ alanBot/index.js
@@ -1,14 +1,17 @@
+
+/* IMPORT MODULES */
 const express = require('express');
+const nodemailer = require('nodemailer');
 const app = express();
 const dfff = require('dialogflow-fulfillment');
 const { PDFDocument, StandardFonts } = require('pdf-lib');
 const fs = require('fs');
 
-
+/* BOT TINGS */
 var admin = require("firebase-admin");
-
 var serviceAccount = require("./config/alan-f9qc-firebase-adminsdk-4nvfu-de157495fc.json");
 
+/* SERVER VALIDATION */
 try {
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
@@ -32,7 +35,10 @@ app.post('/', express.json(), (req, res)=> {
         response: res
     });
 
-// utility functions
+
+/* GENERAL USE UTILITY FUNCTIONS */
+
+// Get current date
 function current_date() {
     const date = new Date()
     let day = date.getDate()
@@ -41,200 +47,231 @@ function current_date() {
     let fullDate = `${month}/${day}/${year}`
     return fullDate
 }
-
 const date = current_date();
 
+// Send email with PDF attachment
+async function send_email_with_attchmt(to, subject, body, filename, filePath) {
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+    //   auth: {
+    //     user: '',    // our fake email
+    //     pass: ''                // our bot key
+    //   }
+    });
+  
+    // read the PDF file from disk
+    let pdf = fs.readFileSync(filePath);
+  
+    // create an email message with attachment
+    let message = {
+      from: 'USF AlanBot', // sender address
+      to: to, // list of receivers
+      subject: subject, // Subject line
+      html: body, // HTML body
+      attachments: [
+        {
+          filename: filename, // filename to be sent as attachment
+          content: pdf, // file content
+          contentType: 'application/pdf' // content type
+        }
+      ]
+    };
+  
+    // send mail with defined transport object
+    let info = await transporter.sendMail(message);
+  
+    console.log('Message sent: %s', info.messageId);
+  }
 
 
-    function gpa(agent) {
+/* INTENTS */
 
-        var u_number = agent.context.get("u_number").parameters['U-ID'];
-        var pin = agent.context.get("u_number").parameters['pin'];
+function gpa(agent) {
 
-        return database.ref('/' + u_number).once('value').then((snapshot) => {
-            const student = snapshot.val();
-            if (student && student.pin_number == pin){
-                var payloadData = {
-                    "richContent":[
-                        [
-                            {
-                                "type": "info",
-                                "title": "Hi " + student.first_name + " " + student.last_name + "!",
-                                "subtitle": "Your GPA is: " + student.gpa
-                            }
-                        ]
-                    ]
-                }
-                agent.add(new dfff.Payload(agent.UNSPECIFIED, payloadData, {sendAsMessage: true, rawPayload: true }))            }
-            else{
-                console.log('its in here');
-                agent.add('Unfortunately, your UID and pin are not valid. Please try again or contact USF IT for help.');
-            }
-        });
+    var u_number = agent.context.get("u_number").parameters['U-ID'];
+    var pin = agent.context.get("u_number").parameters['pin'];
 
-    }
-
-    async function uldp_auto_fill(agent) {
-
-        var u_number = agent.context.get("u-id").parameters['U-ID'];
-        var pin = agent.context.get("pin_number").parameters['pin'];
-    
-        // load empty uldp form
-        const pdfBytes = fs.readFileSync('uldp-form.pdf');
-        const pdfDoc = await PDFDocument.load(pdfBytes);
-        const form = pdfDoc.getForm();
-    
-        // get unpopulated text fields
-        const last_name_field = form.getTextField('Text1');
-        const first_name_field = form.getTextField('Text2');
-        const u_number_field = form.getTextField('Text3');
-        const date_field = form.getTextField('Text6');
-        
-        // get student user from database
-        const snapshot = await database.ref('/' + u_number).once('value');
+    return database.ref('/' + u_number).once('value').then((snapshot) => {
         const student = snapshot.val();
-
-        // if the student has a valid (u-id, pin), populate form fields with their info
-        if (student && student.pin_number == pin && student.uldp == 'FALSE') {
-            last_name_field.setText(student.last_name);
-            first_name_field.setText(student.first_name);
-            u_number_field.setText('U' + student.u_number);
-            date_field.setText(date);
-
-            // check the student's major to check the correct box
-            switch (student.major) {
-                case 'cpe':
-                    const cpe_field = form.getCheckBox('Button7');
-                    cpe_field.check();
-                  break;
-                case 'cs':
-                    const cs_field = form.getCheckBox('Button8');
-                    cs_field.check();
-                  break;
-                case 'cys':
-                    const cys_field = form.getCheckBox('Button9');
-                    cys_field.check();
-                    break;
-                case 'it':
-                    const it_field = form.getCheckBox('Button10');
-                    it_field.check();
-                    break;
-                default:
-                    break;
-              }
-    
-            // save the modified PDF to a file
-            const newPdfBytes = await pdfDoc.save();
-            fs.writeFileSync('auto-fill-uldp.pdf', newPdfBytes);
-    
+        if (student && student.pin_number == pin){
             var payloadData = {
                 "richContent":[
                     [
                         {
-                            "type": "description",
-                            "title": "Hi " + student.first_name + ",",
-                            "text": [
-                              "This link will take you to your auto-filled ULDP form. Please make sure to sign it and check it for any errors before submitting."
-                            ]
+                            "type": "info",
+                            "title": "Hi " + student.first_name + " " + student.last_name + "!",
+                            "subtitle": "Your GPA is: " + student.gpa
+                        }
+                    ]
+                ]
+            }
+            agent.add(new dfff.Payload(agent.UNSPECIFIED, payloadData, {sendAsMessage: true, rawPayload: true }))            }
+        else{
+            console.log('its in here');
+            agent.add('Unfortunately, your UID and pin are not valid. Please try again or contact USF IT for help.');
+        }
+    });
+
+}
+
+async function uldp_auto_fill(agent) {
+
+    var u_number = agent.context.get("u-id").parameters['U-ID'];
+    var pin = agent.context.get("pin_number").parameters['pin'];
+
+    // load empty uldp form
+    const pdfBytes = fs.readFileSync('uldp-form.pdf');
+    const pdfDoc = await PDFDocument.load(pdfBytes);
+    const form = pdfDoc.getForm();
+
+    // get unpopulated text fields
+    const last_name_field = form.getTextField('Text1');
+    const first_name_field = form.getTextField('Text2');
+    const u_number_field = form.getTextField('Text3');
+    const date_field = form.getTextField('Text6');
+    
+    // get student user from database
+    const snapshot = await database.ref('/' + u_number).once('value');
+    const student = snapshot.val();
+
+    // if the student has a valid (u-id, pin), populate form fields with their info
+    if (student && student.pin_number == pin && student.uldp == 'FALSE') {
+        last_name_field.setText(student.last_name);
+        first_name_field.setText(student.first_name);
+        u_number_field.setText('U' + student.u_number);
+        date_field.setText(date);
+
+        // retrieve student's major to check the correct box
+        switch (student.major) {
+            case 'cpe':
+                const cpe_field = form.getCheckBox('Button7');
+                cpe_field.check();
+                break;
+            case 'cs':
+                const cs_field = form.getCheckBox('Button8');
+                cs_field.check();
+                break;
+            case 'cys':
+                const cys_field = form.getCheckBox('Button9');
+                cys_field.check();
+                break;
+            case 'it':
+                const it_field = form.getCheckBox('Button10');
+                it_field.check();
+                break;
+            default:
+                break;
+            }
+
+        // save the modified PDF to a file
+        const newPdfBytes = await pdfDoc.save();
+        fs.writeFileSync('auto-fill-uldp.pdf', newPdfBytes);
+
+        // send email to student
+        send_email_with_attchmt(student.email, 'Your ULDP Form', '<p>Your ULDP form is attached. After verifying that all the information is correct, please initial and sign it before sending it to your advisor.</p><p>- <b>Alan, CSE Chatbot</b></p>', 'auto-fill-uldp.pdf', 'auto-fill-uldp.pdf');
+
+        var payloadData = {
+            "richContent":[
+                [
+                    {
+                        "type": "description",
+                        "title": "Hi " + student.first_name + ",",
+                        "text": [
+                            "I just sent the auto-filled ULDP form to the email address we have on hand for you: " + student.email,
+                            "Please check your spam folder if you are having trouble locating it."
+                        ]
+                    },
+                    {
+                    "options": [
+                        {
+                            "text": "I need help with something else"
                         },
                         {
-                            "type": "button",
-                            "icon": {
-                              "type": "download",
-                              "color": "#006747"
+                            "text": "Bye!"
+                        }
+                        ],
+                        "type": "chips"
+                    }
+                ]
+            ]
+        }
+        agent.add(new dfff.Payload(agent.UNSPECIFIED, payloadData, {sendAsMessage: true, rawPayload: true }))            
+    } else if (student && student.pin_number == pin && student.uldp == 'TRUE') { 
+        var payloadData = {
+            "richContent":[
+                [
+                    {
+                        "type": "description",
+                        "title": "You're already considered an Upper Level student.",
+                        "text": [
+                            "According to our database, you're already an upper level student. You can double check this by navigating to DegreeWorks and verifying that you have the ULDP attribute. If you think this is an error, please contact your advisor.",
+                        ]
+                    },
+                        {
+                        "options": [
+                            {
+                                "text": "Who is my advisor?"
                             },
-                            "text": student.first_name + "'s ULDP Form",
-                            "link": "https://san-francesco.github.io/alanBot/auto-fill-uldp.pdf",
-                          }, 
-                          {
-                            "options": [
-                                {
-                                  "text": "I need help with something else"
-                                },
-                                {
-                                  "text": "Bye!"
-                                }
-                              ],
-                              "type": "chips"
-                          }
-                    ]
+                            {
+                                "text": "Email my advisor"
+                            },
+                            {
+                                "text": "I need something else"
+                            },
+                            {
+                                "text": "Bye!"
+                            }
+                            ],
+                            "type": "chips"
+                        }
                 ]
-            }
-            agent.add(new dfff.Payload(agent.UNSPECIFIED, payloadData, {sendAsMessage: true, rawPayload: true }))            
-        } else if (student && student.pin_number == pin && student.uldp == 'TRUE') { 
-            var payloadData = {
-                "richContent":[
-                    [
-                        {
-                            "type": "description",
-                            "title": "You're already considered an Upper Level student.",
-                            "text": [
-                              "According to our database, you're already an upper level student. You can double check this by navigating to DegreeWorks and verifying that you have the ULDP attribute. If you think this is an error, please contact your advisor.",
-                            ]
-                        },
-                          {
-                            "options": [
-                                {
-                                    "text": "Who is my advisor?"
-                                },
-                                {
-                                  "text": "Email my advisor"
-                                },
-                                {
-                                  "text": "I need something else"
-                                },
-                                {
-                                  "text": "Bye!"
-                                }
-                              ],
-                              "type": "chips"
-                          }
-                    ]
-                ]
-            }
-            agent.add(new dfff.Payload(agent.UNSPECIFIED, payloadData, {sendAsMessage: true, rawPayload: true }))            
+            ]
         }
-        else {
-            var payloadData = {
-                "richContent":[
-                    [
+        agent.add(new dfff.Payload(agent.UNSPECIFIED, payloadData, {sendAsMessage: true, rawPayload: true }))            
+    }
+    else {
+        var payloadData = {
+            "richContent":[
+                [
+                    {
+                        "type": "description",
+                        "title": "Invalid PIN or U Number",
+                        "text": [
+                            "Unfortunately, your UID and pin are not valid. Please try again or contact USF IT for help.",
+                        ]
+                    },
                         {
-                            "type": "description",
-                            "title": "Invalid PIN or U Number",
-                            "text": [
-                              "Unfortunately, your UID and pin are not valid. Please try again or contact USF IT for help.",
-                            ]
-                        },
-                          {
-                            "options": [
-                                {
-                                    "text": "Auto-fill my ULDP Form"
-                                },
-                                {
-                                    "text": "I don't know my PIN or UID"
-                                },
-                                {
-                                  "text": "I need something else"
-                                },
-                                {
-                                  "text": "Bye!"
-                                }
-                              ],
-                              "type": "chips"
-                          }
-                    ]
+                        "options": [
+                            {
+                                "text": "Auto-fill my ULDP Form"
+                            },
+                            {
+                                "text": "I don't know my PIN or UID"
+                            },
+                            {
+                                "text": "I need something else"
+                            },
+                            {
+                                "text": "Bye!"
+                            }
+                            ],
+                            "type": "chips"
+                        }
                 ]
-            }
-            agent.add(new dfff.Payload(agent.UNSPECIFIED, payloadData, {sendAsMessage: true, rawPayload: true }))            
+            ]
         }
-    
+        agent.add(new dfff.Payload(agent.UNSPECIFIED, payloadData, {sendAsMessage: true, rawPayload: true }))            
     }
 
+}
 
-    var intentMap = new Map();
-    intentMap.set('uldp_auto_fill', uldp_auto_fill)
-    intentMap.set('gpa', gpa)
-    agent.handleRequest(intentMap);
+
+var intentMap = new Map();
+intentMap.set('uldp_auto_fill', uldp_auto_fill)
+intentMap.set('gpa', gpa)
+agent.handleRequest(intentMap);
 })
 
 app.listen(3333, ()=>console.log("Server is live at port 3333"));
